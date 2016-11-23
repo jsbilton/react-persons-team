8c7d3dd8bbfbc35acc0e6fd46fc28c04a53232ac
Jared Bilton <jsbilton@gmail.com>
2016-11-23 09:50:32 -0500

    step 1 and 2 for persons

---
+++ src/app.js
@@ -0,0 +1,81 @@
+const React = require('react')
+
+const App = React.createClass({
+  getInitialState: function() {
+    return {
+      persons: [{
+        name: 'Tom',
+        id: 1
+      },
+      {
+        name: 'Trip',
+        id: 2
+      },
+      {
+        name: 'Derek',
+        id: 3
+      },
+      {
+        name: 'Kendra',
+      id: 4
+    },
+      {
+        name: 'Andrea',
+      id: 5
+    },
+      {
+        name: 'Jared',
+      id: 6
+    },
+      {
+        name: 'Joshua',
+      id: 7
+    },
+      {
+        name: 'Chris',
+      id: 8
+    },
+      {
+        name: 'Alex',
+        id: 9
+    },
+      {
+        name: 'Joe',
+        id: 10
+      },
+      {
+        name: 'Cameron',
+        id: 11
+      },
+      {
+        name: 'Matt',
+        id: 12
+      }],
+      members: [{
+        name: 'Tom',
+        id: 1
+      }, {
+      name: 'Trip',
+      id: 2
+    }]
+  }
+},
+  render() {
+    return (
+      <div>
+        <div style={{float: 'left'}}>
+          <h3>Persons</h3>
+          <ul>
+            {this.state.persons.map(p =>
+              <li key={p.id}>{p.name}
+                <button>Add Member</button>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+})
+
+module.exports = App
